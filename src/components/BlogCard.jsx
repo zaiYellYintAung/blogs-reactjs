08b940d8ac36b128ec5dea36603d89dc08508910
Zai <zaiyellyintaung@gmail.com>
2023-05-07 03:52:43 +0630

    left delete and some

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const BlogCard = ({ title, body }) => {
+const BlogCard = ({ title, body, id }) => {
+  const [imageURL, setImageURL] = useState("");
+
+  useEffect(() => {
+    fetch("https://picsum.photos/200/300") // Fetch a random image from the Picsum API
+      .then((response) => {
+        setImageURL(response.url);
+      });
+  }, [id]);
+
   return (
     <div className="card">
+      <img src={imageURL} alt="Random Image" />
       <div className="card-content">
         <div className="media">
           <div className="media-content">
@@ -11,7 +21,7 @@ const BlogCard = ({ title, body }) => {
         </div>
 
         <div className="content">{body}</div>
-        <button className="button is-small">Button </button>
+        <button className="button is-small is-danger">Delete </button>
       </div>
       <hr></hr>
     </div>
--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -3,19 +3,32 @@ import BlogCard from "./BlogCard";
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
+  const [bid, setBid] = useState(0);
+  const handleDelete = () => {};
+
+  const refresh = () => {
+    setBid(bid + 10);
+  };
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
-      .then((data) => setBlogs(data));
-  }, []);
+      .then((data) => {
+        const slicedData = data.slice(0, bid);
+        setBlogs(slicedData);
+      });
+  }, [bid]);
   return (
-    <div>
+    <div className="@">
       {blogs.map((blog) => (
         <div key={blog.id}>
-          <BlogCard title={blog.title} body={blog.body} />
+          <BlogCard title={blog.title} body={blog.body} id={blog.id} />
         </div>
       ))}
+      <div className="button is-hero is-full-width" onClick={refresh}>
+        More
+      </div>
+      <br></br>
     </div>
   );
 };
